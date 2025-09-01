8510aa7297ca1dde5b0e00380f549445efe12247
ohyudan <ohyudan86@gmail.com>
2025-09-01 22:22:40 +0900

    fix: CMakeLists 경로 변경 및 이름 수정

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
-#include "project01/Project01.h"
+#include "ModenCplusplus/project01/Project01.h"
+
 
 int main() {
-    Project01 project01 = Project01();
-    project01.print();
+  Project01 project01 = Project01();
+  project01.print();
 
-    return 0;
+  return 0;
 }
