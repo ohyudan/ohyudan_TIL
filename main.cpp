6c5ac96ebdbc9c552b490227e37cca5890800f56
ohyudan <ohyudan86@gmail.com>
2025-09-02 23:11:34 +0900

    fix: window 환경에서 글자깨짐 해결

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
 
 #include "ModernCplusplus/project01/Project01.h"
-
-
+#ifdef _WIN32
+#include <windows.h>
+struct ConsoleSetup {
+    ConsoleSetup() {
+        SetConsoleOutputCP(CP_UTF8);
+        SetConsoleCP(CP_UTF8);
+    }
+};
+static ConsoleSetup setup;
+#endif
 int main() {
-  Project01 project01 = Project01();
+
+  auto project01 = Project01();
   project01.print();
 
   return 0;
